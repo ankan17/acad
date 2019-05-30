fe813d13b2fb96c94276e7676822be4b30f7a838
Aditya <guptaaditya1908@gmail.com>
2019-05-30 15:32:31 +0530

    app.py updated

--- app.py
+++ app.py
@@ -121,5 +121,7 @@ def logout():
 if __name__ == "__main__":
   	db.create_all()
   	from models import Counter
-  	counter = Counter()
+  	obj = Counter.query.all()
+  	if not obj
+  		counter = Counter()
   	app.run(debug=True)
